d02a0116fa4f101c8f0580682714ea9937936ce1
Adil Sadik <sparky.005@gmail.com>
2022-12-17 21:52:19 -0500

    add theme-ui and header shadowing

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,18 @@
+import theme from '@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui/index'
+
+export default {
+  ...theme,
+  section_hero: {
+//    display: 'none',
+    mb: [4, 5, 6],
+//    p: { fontSize: [1, 2, 3], mt: 2 }
+  },
+  section_bottom: {
+    display: 'none'
+  },
+  header: {
+    mb: [0, 0],
+    mt: [12, 12, 12],
+    display: 'none',
+  }
+}
