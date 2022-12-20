bcefd3943cef1f407bcf0170a0d6370085cdc7c8
Adil Sadik <sparky.005@gmail.com>
2022-12-20 16:06:38 -0500

    add RSS link to navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,10 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
+          {
+            title: `RSS`,
+            slug: `/rss.xml`,
+          },
         ],
         externalLinks: [
           {
