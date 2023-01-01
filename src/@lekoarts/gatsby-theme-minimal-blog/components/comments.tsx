89e30a48d9093b8cae9ff277b3c81d63ec48e973
Adil Sadik <sparky.005@gmail.com>
2022-12-31 22:02:54 -0500

    add comments section with fastcomments

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/comments.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Component } from 'react'
+import { FastCommentsCommentWidget } from 'fastcomments-react'
+
+class Comments extends Component {
+  render() {
+    return <FastCommentsCommentWidget tenantId="a8gAfTebE5e" />
+  }
+}
+
+export default Comments
