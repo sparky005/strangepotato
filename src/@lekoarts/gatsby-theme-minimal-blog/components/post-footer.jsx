141aa8f6d21167bd9dd1ec405027c463e2c17c13
Adil Sadik <sparky.005@gmail.com>
2023-01-08 17:54:33 -0500

    update footer

--- src/@lekoarts/gatsby-theme-minimal-blog/components/post-footer.jsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post-footer.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Text } from "theme-ui"
 const Footer  = ({ post }) => (
     <section>
-    <Text sx={{ fontStyle: `italic`, color: `secondary`, a: {color: `secondary`}, fontsize: [1, 1, 2] }}>If you like this post please consider subscribing via the <a href="rss.xml">RSS feed</a> or <a href="http://eepurl.com/ihpvjb">newsletter</a>. Thanks for reading!</Text>
+    <Text sx={{ fontStyle: `italic`, color: `secondary`, a: {color: `secondary`}, fontsize: [1, 1, 2] }}>If you like this post please consider subscribing via the <a href="rss.xml">RSS feed</a> or <a href="http://eepurl.com/ihpvjb">newsletter</a>. You can also leave a comment below -- it would mean a lot to me! <br/> Thanks for reading!</Text>
     </section>
 )
 
